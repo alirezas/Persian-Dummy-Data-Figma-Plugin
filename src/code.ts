825b744a5a79a84bfecf9d8964eaead5c346b4eb
Alireza Sarabchi <a.sarabchi@gmail.com>
2022-07-25 17:42:38 +0430

    Ready to launch v1, Generating first name, last name and full name,

--- src/code.ts
+++ src/code.ts
@@ -5,23 +5,53 @@ const getRandom = (list: Array<String>) => {
     return list[Math.floor(Math.random() * list.length)]
 }
 
-const doMagic = async (node) => {
+const loadFonts = async (node) => {
     await Promise.all(
         node.getRangeAllFontNames(0, node.characters.length)
-        .map(figma.loadFontAsync)
+            .map(figma.loadFontAsync)
     )
-
-    const name = getRandom(names)
-    const lastName = getRandom(lastNames)
-    const fullName = `${name} ${lastName}`
-    node.characters = fullName
 }
 
-;(async () => {
-    for (const node of figma.currentPage.selection) {
-        if (node.type === 'TEXT') {
-            await doMagic(node)
-        }
+const doMagic = async (node) => {
+    await loadFonts(node)
+
+    let firstName, lastName, result
+
+    switch (figma.command) {
+        case 'fullname':
+            firstName = getRandom(names)
+            lastName = getRandom(lastNames)
+            result = `${firstName} ${lastName}`
+            break;
+        case 'firstname':
+            firstName = getRandom(names)
+            result = firstName
+            break;
+        case 'lastname':
+            lastName = getRandom(lastNames)
+            result = lastName
+            break;
     }
-    figma.closePlugin()
-})()
+
+    node.characters = result
+}
+
+    ; (async () => {
+        if (!figma.currentPage.selection.length) {
+            figma.notify('Select at least one text layer.')
+            figma.closePlugin()
+        } else {
+            const textNodes = figma.currentPage.selection.filter(node => node.type === 'TEXT')
+            if (!textNodes.length) {
+                figma.notify('Select at least one text layer.')
+                figma.closePlugin()
+            }
+
+            for (const node of textNodes) {
+                if (node.type === 'TEXT') {
+                    await doMagic(node)
+                }
+            }
+            figma.closePlugin()
+        }
+    })()
